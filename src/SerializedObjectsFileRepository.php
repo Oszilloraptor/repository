a9dee4e8327770ebab09260e856da2cf8e54afb0
D. S. Richard Richter <richter@vivaldi.net>
2021-09-28 22:46:32 +0200

    feat: implement SerializedObjectsFileRepository

---
+++ src/SerializedObjectsFileRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rikta\Repository;
+
+/**
+ * @template UnserializedObject
+ * @extends FileRepository<UnserializedObject>
+ */
+class SerializedObjectsFileRepository extends FileRepository
+{
+    /**
+     * @param string        $dir            directory that stores the data for this query
+     * @param bool          $cached         should the files be cached?
+     * @param true|string[] $allowedClasses allowed classes for deserializing
+     */
+    public function __construct(string $dir, bool $cached = true, $allowedClasses = true)
+    {
+        parent::__construct($dir, $cached);
+        $this->allowedClasses = $allowedClasses;
+    }
+
+    /**
+     * Unserializes an Item.
+     *
+     * @param UnserializedObject $item
+     */
+    protected function serialize($item): string
+    {
+        return serialize($item);
+    }
+
+    /**
+     * Serializes an Item.
+     *
+     * @return UnserializedObject
+     */
+    protected function unserialize(string $serialized)
+    {
+        return unserialize($serialized, ['allowedClasses' => $this->allowedClasses]);
+    }
+
+    /** @var true|string[] allowed classes for deserializing */
+    private $allowedClasses;
+}
--- tests/SerializedObjectFileRepository.php
+++ tests/SerializedObjectFileRepository.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Rikta\Repository\Tests;
 
-use Rikta\Repository\FileRepository;
 use Rikta\Repository\RepositoryInterface;
+use Rikta\Repository\SerializedObjectsFileRepository;
 
 /**
  * @internal
  * @coversNothing
  */
-final class FileRepositoryFunctionalTest extends AbstractFileRepositoryFunctionalTestCase
+final class SerializedObjectFileRepository extends AbstractFileRepositoryFunctionalTestCase
 {
     protected function getRepositoryInterface(): RepositoryInterface
     {
-        return new FileRepository($this->dir);
+        return new SerializedObjectsFileRepository($this->dir);
     }
 }
